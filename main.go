f6ee0355f24b7a7c732154f43c539c1ea0141db7
Brenda Nakasone <112347092+brendanaka29@users.noreply.github.com>
2023-02-02 12:41:01 -0500

    formatting

--- main.go
+++ main.go
@@ -1,25 +1,142 @@
-// class for rating system
-
+// https://youtu.be/a3YRzhyy3VY
 package main
 
-// import (
-// 	"encoding/json"
-// 	"github.com/gorilla/mux"
-// 	"log"
-// 	"net/http"
-// )
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 
-// func main() {
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
+)
 
-// }
+var SECRET_KEY = []byte("gosecretkey")
 
-type RatingSystem struct {
-	//food item
-	//date - calendar interface?
-	//student object?
-	//rating
-	//name
-	//
+type User struct {
+	FirstName string `json:"firstname" bson:"firstname"`
+	LastName  string `json:"lastname" bson:"lastname"`
+	Email     string `json:"email" bson:"email"`
+	Password  string `json:"password" bson:"password"`
 }
 
+var client *mongo.Client
 
+func getHash(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
+
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	tokenString, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		log.Println("Error in JWT token generation")
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func userSignup(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var user User
+	json.NewDecoder(request.Body).Decode(&user)
+	user.Password = getHash([]byte(user.Password))
+	collection := client.Database("GODB").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+}
+
+func userLogin(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var user User
+	var dbUser User
+	json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("GODB").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := collection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&dbUser)
+
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	userPass := []byte(user.Password)
+	dbPass := []byte(dbUser.Password)
+
+	passErr := bcrypt.CompareHashAndPassword(dbPass, userPass)
+
+	if passErr != nil {
+		log.Println(passErr)
+		response.Write([]byte(`{"response":"Wrong Password!"}`))
+		return
+	}
+	jwtToken, err := GenerateJWT()
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	response.Write([]byte(`{"token":"` + jwtToken + `"}`))
+
+}
+
+func main() {
+
+	//var food string
+	//var userRating string
+
+	//LOGIN
+	log.Println("Starting the application")
+
+	router := mux.NewRouter()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	router.HandleFunc("/api/user/login", userLogin).Methods("POST")
+	router.HandleFunc("/api/user/signup", userSignup).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+	/*
+		//Front end
+		//take name as input
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("What meal would you like to rate?: ")
+		food, _ = reader.ReadString('\n') //food
+
+		//take rating from user and convert it to float
+		reader = bufio.NewReader(os.Stdin)
+		fmt.Printf("Please rate the %v between 1 and 5: ", food)
+		//userRating = string? mynumRating = float
+		userRating, _ = reader.ReadString('\n')
+		mynumRating, _ := strconv.ParseFloat(strings.TrimSpace(userRating), 64)
+
+		//Back end
+		fmt.Printf("\n Thanks for rating %v with %v star rating.\n\n Your rating was recorded in our system at %v", food, mynumRating, time.Now().Format(time.Stamp))
+
+		//writing ratings to data.txt
+		file, err := os.OpenFile("./data.txt", os.O_APPEND|os.O_WRONLY, 0644)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		file.WriteString("Meal: ")
+		file.WriteString(food)
+		file.WriteString("Rating: ")
+		file.WriteString(userRating)
+		file.WriteString("~")
+	*/
+
+}
