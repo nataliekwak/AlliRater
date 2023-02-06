e76981a41f8d6a4fda9fd5e626f755e150a82019
Brenda Nakasone <112347092+brendanaka29@users.noreply.github.com>
2023-02-06 12:55:24 -0500

    trying to create a backend login
    Co-Authored-By: laurennunag <123413966+laurennunag@users.noreply.github.com>

--- main.go
+++ main.go
@@ -12,7 +12,86 @@ import (
 )
 
 func main() {
+	//var valid bool
+	register()
 
+	valid := false
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter username: ")
+	username, _ := reader.ReadString('\n')
+	username = strings.Replace(username, "\n", "", -1)
+
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Println("Enter password: ")
+	password, _ := reader.ReadString('\n')
+
+	file, err := os.Open("login.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	defer file.Close()
+
+	var temp string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		temp = scanner.Text()
+		fmt.Println("this is temp: ", temp)
+		fmt.Println("this is username: ", username)
+		if temp == username {
+			fmt.Println("W")
+		} else {
+			fmt.Println("L")
+		}
+		fmt.Println(fmt.Sprint(strings.Compare(temp, username)))
+		//temp = strings.Replace(temp, "\n", "", -1)
+
+		if valid {
+			if temp == password {
+				fmt.Println("Login successful!")
+			} else {
+				fmt.Println("Wrong password.")
+				break
+			}
+		}
+		if strings.Compare(temp, username) == 0 {
+			valid = true
+			fmt.Println(temp)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("File reading error", err)
+	}
+
+	rating()
+}
+
+func register() {
+	var user string
+	var pass string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter login")
+	user, _ = reader.ReadString('\n')
+
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter password")
+	pass, _ = reader.ReadString('\n')
+
+	fmt.Println("Thanks for registering as a user with AlliRater")
+
+	file, err := os.OpenFile("./login.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	file.WriteString(user)
+	file.WriteString(pass)
+
+}
+
+func rating() {
 	var food string
 	var userRating string
 
@@ -29,7 +108,6 @@ func main() {
 	userRating, _ = reader.ReadString('\n')
 	mynumRating, _ := strconv.ParseFloat(strings.TrimSpace(userRating), 64)
 
-	//Back end
 	fmt.Printf("\n Thanks for rating %v with %v star rating.\n\n Your rating was recorded in our system at %v", food, mynumRating, time.Now().Format(time.Stamp))
 
 	//writing ratings to data.txt
