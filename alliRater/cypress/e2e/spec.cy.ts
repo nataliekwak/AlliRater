a1e570fc8eaad14b6548c39efddd7ef52baafcee
macguirevan <mmcduff@ufl.edu>
2023-02-24 13:12:29 -0500

    Spec Test Visit

--- alliRater/cypress/e2e/spec.cy.ts
+++ alliRater/cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false)
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io')
   })
 })
