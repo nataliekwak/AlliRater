196597c35611c7859f067df3e7afa21370188255
macguirevan <mmcduff@ufl.edu>
2023-02-24 13:16:05 -0500

    Updating Spec

--- alliRater/cypress/e2e/spec.cy.ts
+++ alliRater/cypress/e2e/spec.cy.ts
@@ -1,5 +1,11 @@
 describe('My First Test', () => {
   it('Visits the Kitchen Sink', () => {
     cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
   })
 })
