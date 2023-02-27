d7b39fca08fe4e78490ece897681d9a65a4f8caa
nataliekwak <natalie.ann.kwak@gmail.com>
2023-02-26 22:23:37 -0500

    Authentication & Authorization Front-end Work
    Services created and permissions set up based on different user roles

--- alliRater/src/app/components/log-in/log-in.component.ts
+++ alliRater/src/app/components/log-in/log-in.component.ts
@@ -1,33 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {FormControl, Validators} from '@angular/forms';
+import { AuthService } from '../../_services/auth.service';
+import { StorageService } from '../../_services/storage.service';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
+
 export class LogInComponent {
 
-  user = new FormControl('', [Validators.required]);
-  pass = new FormControl('', [Validators.required]);
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
 
-  constructor(private router: Router){
-    
+  //user = new FormControl('', [Validators.required]);
+  //pass = new FormControl('', [Validators.required]);
+
+  constructor(private authService: AuthService, private storageService: StorageService, private router: Router){}
+
+  ngOnInit(): void {
+    if (this.storageService.isLoggedIn()) 
+    {
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
   }
 
   onSubmit(){
+    const { username, password } = this.form;
+
     this.router.navigate(['homepage'])
+
+    this.authService.login(username, password).subscribe({
+      next: data => {
+        this.storageService.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        this.reloadPage();
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
   }
 
-  getErrorMessage()
-  {
-    if (this.user.hasError('required') || this.pass.hasError('required'))
-    {
-      return 'You must enter a value'
-    }
-
-    return;
-    // ADD : Check is user exists in database
+  reloadPage(): void {
+    window.location.reload();
   }
+
+  // getErrorMessage()
+  // {
+  //   if (this.user.hasError('required') || this.pass.hasError('required'))
+  //   {
+  //     return 'You must enter a value'
+  //   }
+
+  //   return;
+  // }
 }
