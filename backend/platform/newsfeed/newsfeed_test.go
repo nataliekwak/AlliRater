9bb82f399bf0692e4c78cbaa4e50922e43a56dff
laurennunag <laurenn1024@gmail.com>
2023-03-01 10:34:35 -0500

    Update newsfeed_test.go

--- backend/platform/newsfeed/newsfeed_test.go
+++ backend/platform/newsfeed/newsfeed_test.go
@@ -2,14 +2,23 @@ package newsfeed
 
 import "testing"
 
+//use package testing which allows for automated testing of go packages
+
+//use error methods to indicate that the result was not what we intended it to be
 func TestAdd(t *testing.T) {
 	feed := New()
+	//creates a new feed "object"?
 	feed.Add(Item{})
+	//adds empty slice of items
 
-	if len(feed.Items) != 0 {
+	//the length of the items slice should be 1, but if it is not then the item has not been added
+	if len(feed.Items) != 1 {
 		t.Errorf("Item was not added")
 	}
 }
+
+//determines if the get all function works
+//similarly we add an item to our feed
 
 func TestGetAll(t *testing.T) {
 	feed := New()
