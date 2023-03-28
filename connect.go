edc80c66f2f982063d0cebd7c6f47b909c245dae
Brenda Nakasone <112347092+brendanaka29@users.noreply.github.com>
2023-03-28 12:41:16 -0400

    Losing it

---
+++ connect.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"go-admin/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Connect() {
+	database, err := gorm.Open(mysql.Open("mydb_admin:rootroot@/go_admin"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
+	database.AutoMigrate(&models.User{})
+}
