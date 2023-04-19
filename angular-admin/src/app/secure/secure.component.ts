e6ab0c7b3d4be50c86b21a3f18d8db84c1e7a9c1
Brenda Nakasone <112347092+brendanaka29@users.noreply.github.com>
2023-04-19 14:28:28 -0400

    Average Rating Kinda Sorta
    Co-Authored-By: laurennunag <123413966+laurennunag@users.noreply.github.com>

--- angular-admin/src/app/secure/secure.component.ts
+++ angular-admin/src/app/secure/secure.component.ts
@@ -12,15 +12,24 @@ import { FormsModule } from '@angular/forms';
 })
 export class SecureComponent implements OnInit{
   user: User;
-  
+  value: string;
+  num: number;
+  sum = 0;
+  count = 0;
   constructor(
     private authService: AuthService,
     private router: Router
     ){ }
 
-    templateForm(value: any) {
+    templateForm(value: string) {
       console.log(value);
-      alert(JSON.stringify(value));
+      //alert(JSON.stringify(value));
+      this.value = JSON.stringify(value);
+      this.value = this.value.substring(9,10)
+      this.sum += Number(this.value)
+      this.count += 1;
+      this.value = JSON.stringify(this.sum/this.count)
+      //console.log(value[0])
     }
 
   ngOnInit(): void{
