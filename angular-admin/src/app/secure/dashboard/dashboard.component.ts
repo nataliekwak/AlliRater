98ef91b172c4e3161d6b61b750edbad34fbb09d7
Natalie <natalie.ann.kwak@gmail.com>
2023-04-19 16:43:48 -0400

    colors

--- angular-admin/src/app/secure/dashboard/dashboard.component.ts
+++ angular-admin/src/app/secure/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import {Router} from '@angular/router';
 import { User } from 'src/app/interfaces/user';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
